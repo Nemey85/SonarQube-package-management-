460541565d6f06697caf66903a1c42da2f1bbbd9
Nemey85 <105611192+Nemey85@users.noreply.github.com>
2023-07-26 01:51:38 -0500

    Update Jenkins-redhat8-installation.sh
    java version updated

--- JENKINS-INSTALLATION/Jenkins-redhat8-installation.sh
+++ JENKINS-INSTALLATION/Jenkins-redhat8-installation.sh
@@ -15,7 +15,7 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo \
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum upgrade
 # Add required dependencies for the jenkins package
-sudo yum install java-11-openjdk
+sudo yum install java-17-openjdk
 sudo yum install jenkins
 sudo systemctl daemon-reload
 sudo systemctl enable jenkins
